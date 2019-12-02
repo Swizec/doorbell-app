707c78aef56ad01dc82de0f36005710d7b66a5a2
Swizec Teller <swizec@swizec.com>
2019-12-01 16:49:51 -0800

    Full loop to buzzing someone in

--- backend/src/acceptTranscript.ts
+++ backend/src/acceptTranscript.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent } from "aws-lambda";
 import qs from "querystring";
 import twilio from "twilio";
+import { updateItem } from "./dynamodb";
 
 // 5. Send text to swizec
 
@@ -10,19 +11,29 @@ const accountSid = "***REMOVED***";
 const authToken = "***REMOVED***";
 
 export const handler = async (event: APIGatewayEvent) => {
-    const { RecordingUrl, TranscriptionText, CallSid } = qs.parse(event.body!);
+    const { RecordingUrl, TranscriptionText, CallSid, Called } = qs.parse(
+        event.body!
+    );
 
-    console.log(process.env.PHONE_NUMBER);
+    // Save (CallSid, Called) pair for lookup in SMS handler
+    await updateItem({
+        Key: { phone_number: Called as string },
+        UpdateExpression: "SET callSid = :callSid, createdAt = :createdAt",
+        ExpressionAttributeValues: {
+            ":callSid": CallSid as string,
+            ":createdAt": new Date().toISOString()
+        }
+    });
 
     const client = twilio(accountSid, authToken);
     await client.messages.create({
         body: `There's someone at the door!\n\n
-        "${TranscriptionText}"\n\n
+        transcript: "${TranscriptionText}"\n\n
         recording: ${RecordingUrl}\n
         Reply YES to let them in!
         `,
         to: "+16505375963",
-        from: process.env.PHONE_NUMBER
+        from: Called as string
     });
 
     return {
--- backend/src/handleSMSReply.ts
+++ backend/src/handleSMSReply.ts
@@ -1,30 +1,52 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { twiml } from "twilio";
+import twilio from "twilio";
 import qs from "querystring";
 
 import { sendTwiml } from "./utils";
+import { getItem } from "./dynamodb";
 
 // 6. Get sms reply
 // 7. Send dial tone to correct phone call
 
 export const handler = async (event: APIGatewayEvent) => {
     const { Body, To } = qs.parse(event.body!);
+    const { Item } = await getItem({ Key: { phone_number: To as string } });
 
-    if ((Body as string).toLowerCase().includes("yes")) {
-        // open the door
-        return openDoor(To as string);
+    console.log(Item);
+
+    if (Item) {
+        if ((Body as string).toLowerCase().includes("yes")) {
+            // open the door
+            return openDoor(Item.callSid);
+        } else {
+            // don't open the door
+            return closeDoor(Item.callSid);
+        }
     } else {
-        // don't open the door
-        return closeDoor(To as string);
+        const response = new twiml.MessagingResponse();
+        const message = response.message(
+            "No ongoing call. Only text this number in response to a message ✌️"
+        );
+
+        return sendTwiml(message);
     }
 };
 
 // Texts user that door is openinng
 // Hooks into ongoing phone call
 // Sends dial tone
-async function openDoor(phoneNumber: string) {
+async function openDoor(callSid: string) {
+    const success = await continueCall(callSid, true);
+
     const response = new twiml.MessagingResponse();
-    const message = response.message("Letting them in");
+    let message;
+
+    if (success) {
+        response.message("Opened the door");
+    } else {
+        message = response.message("Something went wrong");
+    }
 
     return sendTwiml(message);
 }
@@ -32,9 +54,41 @@ async function openDoor(phoneNumber: string) {
 // Texts user that door is NOT opening
 // Hooks into ongoing nphone call
 // Tells them to go away
-async function closeDoor(phoneNumber: string) {
+async function closeDoor(callSid: string) {
+    const success = await continueCall(callSid, true);
+
     const response = new twiml.MessagingResponse();
-    const message = response.message("Telling them to go away");
+    let message;
+
+    if (success) {
+        message = response.message("Told them to go away");
+    } else {
+        message = response.message("Something went wrong");
+    }
 
     return sendTwiml(message);
+}
+
+// This should come from AWS secrets storage, but I'm lazy
+// will rotate these secrets after the stream
+const accountSid = "***REMOVED***";
+const authToken = "***REMOVED***";
+
+async function continueCall(callSid: string, openDoor: boolean) {
+    const client = twilio(accountSid, authToken);
+    try {
+        const response = new twiml.VoiceResponse();
+        if (openDoor) {
+            response.say("Buzzing you in");
+            response.play({ digits: "w9" });
+        } else {
+            response.say("Sorry, residents said you can't come in");
+        }
+
+        await client.calls(callSid).update({ twiml: response.toString() });
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
 }
